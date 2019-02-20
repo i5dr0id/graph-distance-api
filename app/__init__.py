328c3e2917ba03eb43b608dba58a3edc1627ee52
i5dr0id <i5dr0id308@gmail.com>
2019-02-20 15:02:53 +0100

    percentage_score

--- app/__init__.py
+++ app/__init__.py
@@ -238,5 +238,5 @@ def similarity_score():
 		temp = Levenshtein.ratio('', '')
 		SEDStopTimer = time.time()
 		SEDtotalTimer = SEDStartTimer - SEDStopTimer
-		percentage_score = (score/1) * 100
+		percentage_score = int((score/1) * 100)
 		return jsonify({'sedtime': SEDtotalTimer, 'score':score, 'diff': '', 'percentage_score': percentage_score}), 201
